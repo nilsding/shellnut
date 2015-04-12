22d69269aea0fbb8340d6dde8d14b129e420d945
pixeldesu <git@pixelde.su>
2015-04-12 18:41:56 +0200

    prepare for opensource release, add license, update readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
- 
+# shellnut 
+_Mumble-to-IRC/IRC-to-Mumble Chat Gateway_
+
+## What you need
+
+* Ruby (**>=2.1.0**)
+* A Mumble and an IRC-server to connect to
+
+## Preparations
+
+* Edit and copy `config.yml.example` to `config.yml`
+
+## Usage
+
+`ruby shellnut.rb`
+
+### Commands
+
+* `!mumble` will show a list of all online users on the Mumble and which channel they are in
+
+## Thanks to
+
+* [nilsding](https://github.com/nilsding) for helping me to parse colors from IRC to Mumble
+
+## Licensed
+
+Licensed under the [MIT License](http://opensource.org/licenses/MIT)
--- shellnut.rb
+++ shellnut.rb
@@ -3,7 +3,6 @@
 require "rubygems"
 require "IRC"
 require "mumble-ruby"
-require "pp"
 
 $LOAD_PATH.unshift File.expand_path './lib', File.dirname(__FILE__)
 
@@ -11,14 +10,7 @@ require "irc_colors"
 
 APP_CONFIG = YAML.load_file(File.expand_path("config.yml", File.dirname(__FILE__)))
 
-# Configure all clients globally
 Mumble.configure do |conf|
-  # sample rate of sound (48 khz recommended)
-  conf.sample_rate = 48000
-
-  # bitrate of sound (32 kbit/s recommended)
-  conf.bitrate = 32000
-
   # directory to store user's ssl certs
   conf.ssl_cert_opts[:cert_dir] = File.expand_path("./certs")
 end
@@ -28,7 +20,10 @@ mumble = Mumble::Client.new(APP_CONFIG['mumble']['server'], APP_CONFIG['mumble']
     conf.username = APP_CONFIG['mumble']['username']
 end
 
-irc = IRC.new(APP_CONFIG['irc']['nickname'], APP_CONFIG['irc']['server'], APP_CONFIG['irc']['port'], APP_CONFIG['irc']['realname'])
+irc = IRC.new(APP_CONFIG['irc']['nickname'], 
+              APP_CONFIG['irc']['server'], 
+              APP_CONFIG['irc']['port'], 
+              APP_CONFIG['irc']['realname'])
 
 def start(irc, mumble)
   @irc_thread ||= Thread.new do
@@ -37,10 +32,11 @@ def start(irc, mumble)
         irc.send_message(APP_CONFIG['irc']['channel'], "There are currently #{mumble.users.count} users connected to #{APP_CONFIG['mumble']['server']}")
         unless mumble.users.count == 0
           mumble.users.each do |user|
+            irc.send_message(APP_CONFIG['irc']['channel'], "\x02#{user[1].name.sub("\n", '')}\x02 in \x02#{mumble.channels[user[1].channel_id].name}\x02") 
           end
         end
       else
-        mumble.text_channel(APP_CONFIG['mumble']['channel'], "#{event.from}: #{event.message}".irc_colors)
+        mumble.text_channel(APP_CONFIG['mumble']['channel'], "<b>#{event.from}:</b> #{event.message}".irc_colors)
       end
     }
     IRCEvent.add_callback('endofmotd') { |event|
@@ -51,7 +47,7 @@ def start(irc, mumble)
 
   @mumble_thread ||= Thread.new do
     mumble.on_text_message do |msg|
-      mumble_msg = "#{mumble.users[msg.actor].name.sub("\n", '')}: #{msg.message}"
+      mumble_msg = "\x02#{mumble.users[msg.actor].name.sub("\n", '')}:\x02 #{msg.message}"
       irc.send_message(APP_CONFIG['irc']['channel'], mumble_msg)
     end
 
