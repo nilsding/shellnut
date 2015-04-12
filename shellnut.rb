7ef4d78336f2fcc6e321b7f983ba95869cb70bb0
pixeldesu <git@pixelde.su>
2015-04-12 21:54:10 +0200

    attempt to fix users shown muted

--- shellnut.rb
+++ shellnut.rb
@@ -32,7 +32,7 @@ def start(irc, mumble)
         irc.send_message(APP_CONFIG['irc']['channel'], "There are currently #{mumble.users.count} users connected to #{APP_CONFIG['mumble']['server']}")
         unless mumble.users.count == 0
           mumble.users.each do |user|
-            irc.send_message(APP_CONFIG['irc']['channel'], "\x02#{user[1].name.sub("\n", '')}\x02 in \x02#{mumble.channels[user[1].channel_id].name} #{"\x034[muted]\x0f" if user[1].self_mute?}#{"\x038[deafened]\x0f" if user[1].deafened?}\x02") 
+            irc.send_message(APP_CONFIG['irc']['channel'], "\x02#{user[1].name.sub("\n", '')}\x02 in \x02#{mumble.channels[user[1].channel_id].name} #{"\x034[muted]\x0f" if user[1].self_mute}#{"\x038[deafened]\x0f" if user[1].deafened?}\x02") 
           end
         end
       else
