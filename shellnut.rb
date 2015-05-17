dcf70f6b9f904a78b69d64498bb6f9bd1bf3b855
pixeldesu <git@pixelde.su>
2015-05-17 20:08:05 +0200

    add multichannel support

--- shellnut.rb
+++ shellnut.rb
@@ -8,11 +8,12 @@ $LOAD_PATH.unshift File.expand_path './lib', File.dirname(__FILE__)
 
 require "irc_colors"
 
-VERSION = "0.1.2"
+VERSION = "0.2.0"
 
 APP_CONFIG = YAML.load_file(File.expand_path("config.yml", File.dirname(__FILE__)))
 
 $irc_users = []
+$irc_channel = ""
 
 Mumble.configure do |conf|
   # directory to store user's ssl certs
@@ -35,18 +36,20 @@ def start(irc, mumble)
       unless event.stats[7] == APP_CONFIG['irc']['nickname']
         realname = event.message
         realname.slice! "0 "
+        $irc_channel = event.channel
         $irc_users << {nickname: event.stats[7], realname: realname }
       end
     }
     IRCEvent.add_callback('endofwho') { |event|
       sleep(2)
-      user_msg = "There are currently #{$irc_users.count} users connected to #{APP_CONFIG['irc']['channel']} on #{APP_CONFIG['irc']['server']}<br/>"
+      user_msg = "There are currently #{$irc_users.count} users connected to #{$irc_channel} on #{APP_CONFIG['irc']['server']}<br/>"
       $irc_users.each do |user|
         user_msg += "<b>#{user[:nickname]}</b> (#{user[:realname]}) <br/>"
       end
       mumble.text_channel(APP_CONFIG['mumble']['channel'], user_msg)
       user_msg = ""
       $irc_users = []
+      $irc_channel = ""
     }
     IRCEvent.add_callback('privmsg') { |event|
       if event.message.start_with? "+users"
@@ -68,12 +71,17 @@ def start(irc, mumble)
         irc_msg.slice! "+mumble"
         irc_msg.strip!
         unless irc_msg.empty?
-          mumble.text_channel(APP_CONFIG['mumble']['channel'], "<b>#{event.from}:</b> #{irc_msg}".irc_colors)
+          mumble.text_channel(APP_CONFIG['mumble']['channel'], "<b>#{event.from}(#{event.channel}):</b> #{irc_msg}".irc_colors)
+          puts "[IRC->Mumble] #{event.from}(#{event.channel}): #{irc_msg}"
         end
       end
     }
     IRCEvent.add_callback('endofmotd') { |event|
-      irc.add_channel(APP_CONFIG['irc']['channel'])
+      puts "[IRC] Connected to server #{APP_CONFIG['irc']['server']}"
+      APP_CONFIG['irc']['channel'].each do |channel|
+        irc.add_channel(channel)
+        puts "[IRC] Joined channel: #{channel}"
+      end
     }
     irc.connect
   end
@@ -90,15 +98,32 @@ def start(irc, mumble)
         mumble_msg = msg.message
         mumble_msg.slice! "+irc"
         mumble_msg.strip!
-        unless mumble_msg.empty?
-          irc.send_message(APP_CONFIG['irc']['channel'], "\x02#{mumble.users[msg.actor].name.sub("\n", '')}:\x02 #{mumble_msg}")
+        mumble_msg = mumble_msg.gsub(/\s+/m, ' ').split(" ")
+        mumble_chl = mumble_msg[0]
+        mumble_msg.delete_at(0)
+        mumble_msg = mumble_msg.join " "
+        if APP_CONFIG['irc']['channel'].include? mumble_chl
+          unless mumble_msg.empty?
+            irc.send_message(mumble_chl, "\x02#{mumble.users[msg.actor].name.sub("\n", '')}:\x02 #{mumble_msg}")
+            puts "[Mumble->IRC] #{mumble.users[msg.actor].name.sub("\n", '')}: #{mumble_msg}"
+          end
+        else
+          mumble.text_user(msg.actor, "Error: Invalid Channel")
         end
       elsif msg.message.start_with? "+users"
-        IRCConnection.send_to_server "WHO #{APP_CONFIG['irc']['channel']}"
+        mumble_chl = msg.message
+        mumble_chl.slice! "+users"
+        mumble_chl.strip!
+        if APP_CONFIG['irc']['channel'].include? mumble_chl
+          IRCConnection.send_to_server "WHO #{mumble_chl}"
+        else
+          mumble.text_user(msg.actor, "Error: Invalid Channel")
+        end
       end
     end
 
     mumble.connect
+    puts "[Mumble] Connected to server #{APP_CONFIG['mumble']['server']}"
 
     mumble.on_connected do
       mumble.me.mute
@@ -107,6 +132,7 @@ def start(irc, mumble)
 
     sleep(2)
     mumble.join_channel(APP_CONFIG['mumble']['channel'])
+    puts "[Mumble] Joined channel: #{APP_CONFIG['mumble']['channel']}"
   end
 
   @irc_thread.join
